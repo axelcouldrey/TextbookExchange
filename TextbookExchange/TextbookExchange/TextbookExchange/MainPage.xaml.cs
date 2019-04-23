595566f5257f244a1e851c189ccffed64c8d2df0
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2019-04-24 10:35:44 +1200

    Created Xamarin UI Storyboard
    Working on AddBook.xaml & AddBook.xamls.
    Note if .xaml and .cs are not compile e.g .cs is nested in .xaml, then:
    
    I don't know of a way to do it in visual studio, but you can edit your .csproj file in a text editor. You should find something like this:
    
    <Page Include="MainWindow.xaml">
      <Generator>MSBuild:Compile</Generator>
      <SubType>Designer</SubType>
    </Page>
    <Compile Include="MainWindow.xaml.cs">
      <DependentUpon>MainWindow.xaml</DependentUpon>
      <SubType>Code</SubType>
    </Compile>

--- TextbookExchange/TextbookExchange/TextbookExchange/MainPage.xaml.cs
+++ TextbookExchange/TextbookExchange/TextbookExchange/MainPage.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace TextbookExchange
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        /*async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }*/
     }
 }
---
+++ TextbookExchange/TextbookExchange/TextbookExchange/UserEnvironment.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TextbookExchange
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserEnvironment : TabbedPage
+    {
+        public UserEnvironment()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            //  TODO: Log out
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+
+        async void OnAddBookButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new AddBook(), this);
+            await Navigation.PopAsync();
+        }
+    }
+}
